af832cd828b0b66f86f88bc0caee1218ece9de4f
Daniel Briceno <daniel.b100726@gmail.com>
2022-12-20 11:08:23 -0400

    Class 23: Vue progresivo

---
+++ class_22/Hola.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "Hello Vue"
+    }
+  }
+}
+</script>
+
+<style>
+p {
+  color: red;
+}
+</style>
